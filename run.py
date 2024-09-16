e1ae1e964bd0aaea378ba3afe7a86b8eba6c7e86
Ayush Shridhar <ayushshridhar@google.com>
2024-09-16 16:33:00 -0700

    groq

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ import argparse
 
 from tot.tasks import get_task
 from tot.methods.bfs import solve, naive_solve
-from tot.models import gpt_usage
+#from tot.models import gpt_usage
 
 def run(args):
     task = get_task(args.task)
@@ -24,7 +24,7 @@ def run(args):
 
         # log
         infos = [task.test_output(i, y) for y in ys]
-        info.update({'idx': i, 'ys': ys, 'infos': infos, 'usage_so_far': gpt_usage(args.backend)})
+        info.update({'idx': i, 'ys': ys, 'infos': infos, 'usage_so_far': -1})
         logs.append(info)
         with open(file, 'w') as f:
             json.dump(logs, f, indent=4)
@@ -37,12 +37,12 @@ def run(args):
     
     n = args.task_end_index - args.task_start_index
     print(cnt_avg / n, cnt_any / n)
-    print('usage_so_far', gpt_usage(args.backend))
+    print('usage_so_far', -1)#gpt_usage(args.backend))
 
 
 def parse_args():
     args = argparse.ArgumentParser()
-    args.add_argument('--backend', type=str, choices=['gpt-4', 'gpt-3.5-turbo'], default='gpt-4')
+    args.add_argument('--backend', type=str, choices=['llama-3.1-70b-versatile'], default='llama-3.1-70b-versatile')
     args.add_argument('--temperature', type=float, default=0.7)
 
     args.add_argument('--task', type=str, required=True, choices=['game24', 'text', 'crosswords'])
